67818c529014fe1ae9038d1e05ae8ef214babe5f
huynguyen2k <huynguyen235023@gmail.com>
2022-12-16 11:04:06 +0700

    Adding meta tags for page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import { MetaTags } from '@/components/common/MetaTags'
 import { MainLayout } from '@/components/layout'
 
 export default function HomePage() {
-  return <>Home page</>
+  return (
+    <>
+      <MetaTags
+        title="Spotify - Web Player"
+        description="Spotify is a digital music service that gives you access to millions of songs."
+        url="https://open.spotify.com/"
+        image="https://open.spotifycdn.com/cdn/images/og-image.548bc4b7.png"
+      />
+    </>
+  )
 }
 
 HomePage.Layout = MainLayout
