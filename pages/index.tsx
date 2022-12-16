f36dde63aaf9d25c4c5b13dc92660d5ceae40396
huynguyen2k <huynguyen235023@gmail.com>
2022-12-16 14:57:49 +0700

    Update vercel url

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
         title="Spotify - Web Player"
         description="Spotify is a digital music service that gives you access to millions of songs."
         url={`${process.env.NEXT_PUBLIC_VERCEL_URL}`}
-        image={`${process.env.NEXT_PUBLIC_VERCEL_URL}spotify-image.png`}
+        image={`${process.env.NEXT_PUBLIC_VERCEL_URL}/spotify-image.png`}
       />
     </>
   )
