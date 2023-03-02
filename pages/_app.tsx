562d6d42ae5c4b65512e1c0d20289f6c43ca9856
huynguyen2k <huynguyen235023@gmail.com>
2023-03-02 11:04:02 +0700

    Setup react-query for the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import { ReactElement, ReactNode } from 'react'
+import { ReactElement, ReactNode, useState } from 'react'
 import Head from 'next/head'
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { EmptyLayout } from '@/components/layout'
 import '../styles/globals.css'
 
@@ -18,23 +20,30 @@ export type AppPropsWithLayout = AppProps & {
 }
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const [queryClient] = useState(() => new QueryClient())
   const Layout = Component.Layout ?? EmptyLayout
 
   return (
     <>
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Head>
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/favicon/site.webmanifest" />
+            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+          </Head>
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Hydrate>
+
+        <ReactQueryDevtools initialIsOpen={false} panelPosition="right" />
+      </QueryClientProvider>
     </>
   )
 }
