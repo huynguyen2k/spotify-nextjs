9fe8484e67b78d811464632353bd156a3a1d7821
huynguyen2k <huynguyen235023@gmail.com>
2023-03-19 09:43:40 +0700

    Setup next-i18next for the project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { i18n } = require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -11,6 +13,7 @@ const nextConfig = {
 
     return config
   },
+  i18n,
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { ReactElement, ReactNode, useState } from 'react'
+import { appWithTranslation } from 'next-i18next'
 import { EmptyLayout } from '@/components/layout'
 import { AppFavicon } from '@/components/common'
 import '../styles/globals.css'
@@ -19,7 +20,7 @@ export type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+function App({ Component, pageProps }: AppPropsWithLayout) {
   const [queryClient] = useState(() => new QueryClient())
 
   const Layout = Component.Layout ?? EmptyLayout
@@ -40,3 +41,5 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     </>
   )
 }
+
+export default appWithTranslation(App)
